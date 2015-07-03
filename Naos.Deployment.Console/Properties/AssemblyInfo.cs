8a496587c5c54922c406e3530ebe600b4bd1c150
Lawson Caudill <lawson@caudillclan.us>
2015-07-03 17:00:44 -0400

    Updated packages and everything (except the console app) to be CLS Compliant True

--- Naos.Deployment.Console/Properties/AssemblyInfo.cs
+++ Naos.Deployment.Console/Properties/AssemblyInfo.cs
@@ -4,6 +4,7 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -40,3 +41,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(false)]
