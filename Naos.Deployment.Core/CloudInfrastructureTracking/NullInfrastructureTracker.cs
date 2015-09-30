c9fd6148553c86bd47eeabc6d7037db6bc9066e1
Lawson Caudill <lawson@caudillclan.us>
2015-09-29 22:50:01 -0300

    Made some null implementations and move stuff into folders to make testing easier.

---
+++ Naos.Deployment.Core/CloudInfrastructureTracking/NullInfrastructureTracker.cs
@@ -0,0 +1,72 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NullInfrastructureTracker.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Deployment.Core.CloudInfrastructureTracking
+{
+    using System.Collections.Generic;
+
+    using Naos.Deployment.Contract;
+
+    /// <summary>
+    /// Null object implementation for testing.
+    /// </summary>
+    public class NullInfrastructureTracker : ITrackComputingInfrastructure
+    {
+        /// <inheritdoc />
+        public ICollection<InstanceDescription> GetInstancesByDeployedPackages(string environment, ICollection<PackageDescription> packages)
+        {
+            return new List<InstanceDescription>();
+        }
+
+        /// <inheritdoc />
+        public void ProcessInstanceTermination(string environment, string systemId)
+        {
+        }
+
+        /// <inheritdoc />
+        public InstanceCreationDetails GetNewInstanceCreationDetails(
+            string environment,
+            DeploymentConfiguration deploymentConfiguration,
+            ICollection<PackageDescription> intendedPackages)
+        {
+            return new InstanceCreationDetails();
+        }
+
+        /// <inheritdoc />
+        public void ProcessInstanceCreation(InstanceDescription instanceDescription)
+        {
+        }
+
+        /// <inheritdoc />
+        public void ProcessDeployedPackage(string environment, string systemId, PackageDescription package)
+        {
+        }
+
+        /// <inheritdoc />
+        public InstanceDescription GetInstanceDescriptionById(string environment, string systemId)
+        {
+            return null;
+        }
+
+        /// <inheritdoc />
+        public string GetInstanceIdByName(string environment, string name)
+        {
+            return null;
+        }
+
+        /// <inheritdoc />
+        public string GetPrivateKeyOfInstanceById(string environment, string systemId)
+        {
+            return null;
+        }
+
+        /// <inheritdoc />
+        public string GetDomainZoneId(string environment, string domain)
+        {
+            return null;
+        }
+    }
+}
