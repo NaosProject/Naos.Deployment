c25e86d5517e6b68568be9296177fd9ad7da0fb9
Suraj Gupta <SurajGupta@users.noreply.github.com>
2025-02-13 18:53:25 -0500

    CA fixes

--- Naos.Deployment.Core.Test/Operations/BackupAndPersistDatabaseOpTest.cs
+++ Naos.Deployment.Core.Test/Operations/BackupAndPersistDatabaseOpTest.cs
@@ -7,12 +7,16 @@
 namespace Naos.Deployment.Core.Test
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
     using Naos.SqlServer.Domain;
     using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.CodeAnalysis.Recipes;
     using OBeautifulCode.Type;
 
     public static partial class BackupAndPersistDatabaseOpTest
     {
+        [SuppressMessage("Microsoft.Maintainability", "CA1505:AvoidUnmaintainableCode", Justification = ObcSuppressBecause.CA1505_AvoidUnmaintainableCode_DisagreeWithAssessment)]
+        [SuppressMessage("Microsoft.Performance", "CA1810:InitializeReferenceTypeStaticFieldsInline", Justification = ObcSuppressBecause.CA1810_InitializeReferenceTypeStaticFieldsInline_FieldsDeclaredInCodeGeneratedPartialTestClass)]
         static BackupAndPersistDatabaseOpTest()
         {
         }
--- Naos.Deployment.Core.Test/Operations/DownloadAndRestoreDatabaseOpTest.cs
+++ Naos.Deployment.Core.Test/Operations/DownloadAndRestoreDatabaseOpTest.cs
@@ -7,17 +7,21 @@
 namespace Naos.Deployment.Core.Test
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
     using FakeItEasy;
     using Naos.Deployment.Domain;
     using Naos.FileJanitor.Domain;
     using Naos.SqlServer.Domain;
     using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.CodeAnalysis.Recipes;
     using OBeautifulCode.CodeGen.ModelObject.Recipes;
     using OBeautifulCode.Type;
     using static System.FormattableString;
 
     public static partial class DownloadAndRestoreDatabaseOpTest
     {
+        [SuppressMessage("Microsoft.Maintainability", "CA1505:AvoidUnmaintainableCode", Justification = ObcSuppressBecause.CA1505_AvoidUnmaintainableCode_DisagreeWithAssessment)]
+        [SuppressMessage("Microsoft.Performance", "CA1810:InitializeReferenceTypeStaticFieldsInline", Justification = ObcSuppressBecause.CA1810_InitializeReferenceTypeStaticFieldsInline_FieldsDeclaredInCodeGeneratedPartialTestClass)]
         static DownloadAndRestoreDatabaseOpTest()
         {
             ConstructorArgumentValidationTestScenarios
